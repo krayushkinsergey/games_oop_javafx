9ee12e1e9be44fb30ae97f59581fe268b18adc19
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-10-15 11:08:17 +0300

    3. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -39,7 +39,7 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY()) ? true : false;
+        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY());
     }
 
     @Override
